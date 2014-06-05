5f5f3dd4cf1af24638bd138cea6c7436ab7b6361
swainn <nathan.swain@byu.net>
2014-06-05 10:10:15 -0700

    started the tethys_deploy mods.

--- deployment/tethys_deploy/startup.sh
+++ deployment/tethys_deploy/startup.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+# Declare variables ----------------------------------------------------------#
+cd /tmp/
+CKAN_DATABASE=$(grep -Po "(?<=^CKAN_DATABASE ).*" tethys_deploy.conf)
+CKAN_USER=$(grep -Po "(?<=^CKAN_USER ).*" tethys_deploy.conf)
+CKAN_PASS=$(grep -Po "(?<=^CKAN_PASS ).*" tethys_deploy.conf)
+DATASTORE_DATABASE=$(grep -Po "(?<=^DATASTORE_DATABASE ).*" tethys_deploy.conf)
+DATASTORE_USER=$(grep -Po "(?<=^DATASTORE_USER ).*" tethys_deploy.conf)
+DATASTORE_PASS=$(grep -Po "(?<=^DATASTORE_PASS ).*" tethys_deploy.conf)
+APPS_MANAGER_DATABASE=$(grep -Po "(?<=^APPS_MANAGER_DATABASE ).*" tethys_deploy.conf)
+APPS_MANAGER_USER=$(grep -Po "(?<=^APPS_MANAGER_USER ).*" tethys_deploy.conf)
+APPS_MANAGER_PASS=$(grep -Po "(?<=^APPS_MANAGER_PASS ).*" tethys_deploy.conf)
+rm /tmp/tethys_deploy.conf
+
 # Modify the CKAN config file to reflect linked ports ------------------------#
 sed "s/#solr_url.*/solr_url = http:\/\/$DATA_PORT_8080_TCP_ADDR:$DATA_PORT_8080_TCP_PORT\/solr/g" -i $CKAN_INI
-sed "s/sqlalchemy.url.*/sqlalchemy.url = postgresql:\/\/$PGUSER:$PGPASS@$DATA_PORT_5432_TCP_ADDR:$DATA_PORT_5432_TCP_PORT\/$PGDATABASE/g" -i $CKAN_INI
-sed "s/#ckan.datastore.write_url.*/ckan.datastore.write_url = postgresql:\/\/$PGUSER:$PGPASS@$DATA_PORT_5432_TCP_ADDR:$DATA_PORT_5432_TCP_PORT\/$DSDATABASE/g" -i $CKAN_INI
-sed "s/#ckan.datastore.read_url.*/ckan.datastore.read_url = postgresql:\/\/$DSUSER:$DSPASS@$DATA_PORT_5432_TCP_ADDR:$DATA_PORT_5432_TCP_PORT\/$DSDATABASE/g" -i $CKAN_INI
+sed "s/sqlalchemy.url.*/sqlalchemy.url = postgresql:\/\/$CKAN_USER:$CKAN_PASS@$DATA_PORT_5432_TCP_ADDR:$DATA_PORT_5432_TCP_PORT\/$CKAN_DATABASE/g" -i $CKAN_INI
+sed "s/#ckan.datastore.write_url.*/ckan.datastore.write_url = postgresql:\/\/$CKAN_USER:$CKAN_PASS@$DATA_PORT_5432_TCP_ADDR:$DATA_PORT_5432_TCP_PORT\/$DATASTORE_DATABASE/g" -i $CKAN_INI
+sed "s/#ckan.datastore.read_url.*/ckan.datastore.read_url = postgresql:\/\/$DATASTORE_USER:$DATASTORE_PASS@$DATA_PORT_5432_TCP_ADDR:$DATA_PORT_5432_TCP_PORT\/$DATASTORE_DATABASE/g" -i $CKAN_INI
 
 # Modify the Tethys Apps config file to reflect linked ports -----------------#
-sed "s/tethys.database_manager_url =.*/tethys.database_manager_url = postgresql:\/\/$APP_MANAGER_USER:$APP_MANAGER_PASS@$DATA_PORT_5432_TCP_ADDR:$DATA_PORT_5432_TCP_PORT\/$APP_MANAGER_DATABASE/g" -i $TETHYS_INI
+sed "s/tethys.database_manager_url =.*/tethys.database_manager_url = postgresql:\/\/$APPS_MANAGER_USER:$APPS_MANAGER_PASS@$DATA_PORT_5432_TCP_ADDR:$DATA_PORT_5432_TCP_PORT\/$APPS_MANAGER_DATABASE/g" -i $TETHYS_INI
 
 # Activate the virtual environment -------------------------------------------#
 . $VENV_ACTIVATE
