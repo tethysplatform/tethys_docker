8497561d52cd77ac05334b5caa7140bb010e5cb9
swainn <nathan.swain@byu.net>
2014-06-09 09:25:32 -0700

    Latest

--- deployment/tethys_deploy/scripts/install_apps.sh
+++ deployment/tethys_deploy/scripts/install_apps.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+# Notify
+echo "Installing Apps..."
 
-# Loop through diretories
-for d in $APPS_PROJECTS_DEV/*/ ; do
-    echo "$d"
+# Activate virtual environment
+. $VENV_ACTIVATE
+which python
+
+# Loop through diretories in APPS_PROJECTS_DEV
+for directory in $APPS_PROJECTS_DEV/*/ ; do
+
+    # Check for appropriately named directories (only "ckanapp-" prefix)
+    if [[ "$directory" != ckanapp-* ]]
+    then
+        # Run the installation script
+        cd "$directory"
+        python setup.py install
+    fi
+
 done
--- deployment/tethys_deploy/scripts/startup.sh
+++ deployment/tethys_deploy/scripts/startup.sh
@@ -28,24 +28,22 @@ sed "s/tethys.database_manager_url =.*/tethys.database_manager_url = postgresql:
 . $VENV_ACTIVATE
 
 # Install apps ---------------------------------------------------------------#
-#python /usr/lib/ckan/scripts/install_apps.py $APPS_PROJECTS_DEV
 /usr/lib/ckan/scripts/install_apps.sh
 
+Activate initialize the database -------------------------------------------#
+echo "Initializing CKAN Database..."
+cd $VENV_HOME/src/ckan
+paster db init -c $CKAN_INI
 
-# Activate initialize the database -------------------------------------------#
-#echo "Initializing CKAN Database..."
-#cd $VENV_HOME/src/ckan
-#paster db init -c $CKAN_INI
-#
-## Permissions
-#chown www-data:www-data /var/lib/ckan/default
-#chmod u+rwx /var/lib/ckan/default
-#chown -R www-data:www-data /usr/lib/ckan/default/
-#
-## Start apache
-#/etc/init.d/apache2 start
-#
-## Start nginx (with supervisor)
-#/usr/bin/supervisord
+# Permissions
+chown www-data:www-data /var/lib/ckan/default
+chmod u+rwx /var/lib/ckan/default
+chown -R www-data:www-data /usr/lib/ckan/default/
+
+# Start apache
+/etc/init.d/apache2 start
+
+# Start nginx (with supervisor)
+/usr/bin/supervisord
 
 bash
