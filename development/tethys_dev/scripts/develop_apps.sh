fc55449845f70d92c6025c257902e32460146bf9
swainn <nathan.swain@byu.net>
2014-06-18 11:57:40 -0700

    Updating the dev docker images with lessons learned from deployment dockers.

---
+++ development/tethys_dev/scripts/develop_apps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Notify
+echo "Installing Apps..."
+
+# Activate virtual environment
+. $VENV_ACTIVATE
+which python
+
+# Loop through diretories in APPS_PROJECTS_DEV
+for directory in $APPS_PROJECTS_DEV/*/ ; do
+
+    # Check for appropriately named directories (only "ckanapp-" prefix)
+    if [[ "$directory" != ckanapp-* ]]
+    then
+        # Run the installation script
+        cd "$directory"
+        python setup.py develop
+    fi
+
+done
