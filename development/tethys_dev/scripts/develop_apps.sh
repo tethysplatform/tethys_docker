b9cc67fd263b0f8406a5530711e1f1bce8115e31
swainn <nathan.swain@byu.net>
2014-06-19 11:45:16 -0700

    Fixed bug in the startup.sh of dev docker.

--- development/tethys_dev/scripts/develop_apps.sh
+++ development/tethys_dev/scripts/develop_apps.sh
@@ -4,7 +4,6 @@ echo "Installing Apps..."
 
 # Activate virtual environment
 . $VENV_ACTIVATE
-which python
 
 # Loop through diretories in APPS_PROJECTS_DEV
 for directory in $APPS_PROJECTS_DEV/*/ ; do
@@ -15,6 +14,10 @@ for directory in $APPS_PROJECTS_DEV/*/ ; do
         # Run the installation script
         cd "$directory"
         python setup.py develop
+
+        # Toggle virtualenv activation
+        deactivate
+        . $VENV_ACTIVATE
     fi
 
 done
