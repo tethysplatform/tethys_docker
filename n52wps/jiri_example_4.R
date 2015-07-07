e94b427848dd362bb77d4354abb95a69260a8b22
jirikadlec2 <jirikadlec2@gmail.com>
2015-07-06 19:06:58 -0600

    Restored original Dockerfile

--- n52wps/jiri_example_4.R
+++
@@ -1,6 +0,0 @@
-# wps.des: jiri-example-4, title = jiri-example-4, abstract = jiri-example-4;
-# wps.in: input, integer; 
-# wps.out: output, double; 
-
-# calculate something...
-output = sin(input)
