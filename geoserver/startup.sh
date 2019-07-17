0b38115da13fe25c48a6ec649699390aa53719a2
swainn <nswain@aquaveo.com>
2019-07-17 01:44:54 -0600

    Upgraded geoserver.war and plugins to 2.15.2. Updated Dockerfile for new version. Need to update some template files still (e.g. template_server.xml)

--- geoserver/startup.sh
+++ geoserver/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Generate the geoserver_startup.sh
-python /tmp/gen_runtime_dynamic_files.py
+# Generate the supervisor configs
+python $GEOSERVER_HOME/generated/gen_runtime_dynamic_files.py
 
 # Start with supervisor -----------------------------------------------------------------------------------------------#
 /usr/bin/supervisord -c $GEOSERVER_HOME/supervisord.conf
