f1720f6adcca937247f25281bea8bc0b9c489c36
Nathan Swain <nswain@aquaveo.com>
2022-01-11 16:45:11 -0700

    Use python3 in startup.sh

--- geoserver/startup.sh
+++ geoserver/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate the geoserver_startup.sh
-python /tmp/gen_runtime_dynamic_files.py
+python3 /tmp/gen_runtime_dynamic_files.py
 
 # Start with supervisor -----------------------------------------------------------------------------------------------#
 /usr/bin/supervisord -c $GEOSERVER_HOME/supervisord.conf
